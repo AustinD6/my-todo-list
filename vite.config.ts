6afc4ab67fb019f51898a5da151e288fe5601a98
Austin <2950624017@qq.com>
2026-04-24 17:10:12 +0800

    feat: add Capacitor and PWA support

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: '简约备忘录',
+        short_name: '我的备忘',
+        description: '一个高颜值的现代待办事项应用',
+        theme_color: '#764ba2',
+        icons: [
+          {
+            src: 'favicon.svg',
+            sizes: '192x192',
+            type: 'image/svg+xml'
+          },
+          {
+            src: 'favicon.svg',
+            sizes: '512x512',
+            type: 'image/svg+xml'
+          }
+        ]
+      }
+    })
+  ],
   base: '/my-todo-list/',
 })
